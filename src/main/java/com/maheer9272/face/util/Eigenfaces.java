1d301767f558d6d04956274463e40972f80e0886
Shaik Maheer <maheer9272@gmail.com>
2025-07-17 15:22:01 +0530

    EigenFaces and Face detector

--- src/main/java/com/maheer9272/face/util/Eigenfaces.java
+++ src/main/java/com/maheer9272/face/util/Eigenfaces.java
@@ -6,14 +6,28 @@ import org.apache.commons.math3.linear.ArrayRealVector;
 import java.util.List;
 
 public class Eigenfaces {
+
     public double calculateDistance(Face face1, Face face2) {
+        if (face1 == null || face2 == null) {
+            throw new IllegalArgumentException("Face objects cannot be null");
+        }
+        if (face1.getFeatures() == null || face2.getFeatures() == null) {
+            throw new IllegalArgumentException("Feature vectors cannot be null");
+        }
+        if (face1.getFeatures().length != face2.getFeatures().length) {
+            throw new IllegalArgumentException("Feature vectors must have equal length");
+        }
         ArrayRealVector v1 = new ArrayRealVector(face1.getFeatures());
         ArrayRealVector v2 = new ArrayRealVector(face2.getFeatures());
-        return v1.getDistance(v2); // Euclidean distance for TSCD Stage 2
+        return v1.getDistance(v2);// Calculate Euclidean distance between two feature vectors
     }
 
     public void computePCA(List<Face> faces) {
-        // Placeholder for PCA computation (to be implemented on Days 12-15)
+        if (faces == null || faces.isEmpty()) {
+            throw new IllegalArgumentException("Face list cannot be null or empty");
+        }
+        // Placeholder for PCA computation using Apache Commons Math (RealMatrix for covariance, eigenvalue decomposition)
+        // This is where you would implement the PCA logic, such as calculating the covariance matrix,
         System.out.println("Computing PCA for " + faces.size() + " faces...");
     }
 }
--- src/main/java/com/maheer9272/face/util/FaceDetector.java
+++ src/main/java/com/maheer9272/face/util/FaceDetector.java
@@ -1,7 +1,5 @@
 package com.maheer9272.face.util;
 
-
-
 import com.clarifai.channel.ClarifaiChannel;
 import com.clarifai.credentials.ClarifaiCallCredentials;
 import com.clarifai.grpc.api.*;
@@ -10,6 +8,8 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import com.google.protobuf.ByteString;
 import com.maheer9272.face.model.Image;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FaceDetector {
     private final V2Grpc.V2BlockingStub client;
@@ -17,11 +17,20 @@ public class FaceDetector {
     public FaceDetector() {
         Dotenv dotenv = Dotenv.load();
         String pat = dotenv.get("CLARIFAI_PAT");
+        if (pat == null || pat.isEmpty()) {
+            throw new IllegalStateException("CLARIFAI_PAT is not set in .env file");
+        }
         client = V2Grpc.newBlockingStub(ClarifaiChannel.INSTANCE.getGrpcChannel())
                 .withCallCredentials(new ClarifaiCallCredentials(pat));
     }
 
-    public void detectFaces(Image image) throws Exception {
+    public List<BoundingBox> detectFaces(Image image) throws Exception {
+        if (image == null || image.getPath() == null || image.getFaceId() == null) {
+            throw new IllegalArgumentException("Image and its properties cannot be null");
+        }
+        if (!Files.exists(Paths.get(image.getPath()))) {
+            throw new IllegalArgumentException("Image file does not exist: " + image.getPath());
+        }
         byte[] imageData = Files.readAllBytes(Paths.get(image.getPath()));
         MultiOutputResponse response = client.postModelOutputs(
                 PostModelOutputsRequest.newBuilder()
@@ -29,13 +38,16 @@ public class FaceDetector {
                         .addInputs(Input.newBuilder().setData(
                                 Data.newBuilder().setImage(com.clarifai.grpc.api.Image.newBuilder().setBase64(ByteString.copyFrom(imageData)))))
                         .build());
+        List<BoundingBox> boundingBoxes = new ArrayList<>();
         for (Output output : response.getOutputsList()) {
             for (Region region : output.getData().getRegionsList()) {
                 BoundingBox box = region.getRegionInfo().getBoundingBox();
+                boundingBoxes.add(box);
                 System.out.println("Face detected in " + image.getFaceId() + ": Top=" + box.getTopRow() +
                         ", Left=" + box.getLeftCol() + ", Bottom=" + box.getBottomRow() +
                         ", Right=" + box.getRightCol());
             }
         }
+        return boundingBoxes; // Return for use in Visualizer or Eigenfaces
     }
 }
